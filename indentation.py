e49fbaa3a92820a1e1f2f9fa7bc0e49ee685f482
Gauav Rege <gauravarege@gmail.com>
2020-09-29 17:29:45 +0530

    Add files via upload

---
+++ indentation.py
@@ -0,0 +1,15 @@
+root@server:~/Python_Scripts# cat indentation.py
+## '''  are used for comments.
+'''
+Welcome to Python Scripts
+this is test print
+test
+'''
+print("Hello World!")
+if 3 != 10:
+    print("this is wrong")
+else:
+    print("OK")
+exit()
+
+root@server:~/Python_Scripts#
