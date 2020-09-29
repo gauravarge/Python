0129fbdaf523ca0ccf688fa6473f118a9ff5bbf0
Gauav Rege <gauravarege@gmail.com>
2020-09-29 17:31:32 +0530

    Add files via upload

---
+++ escape_sequance.py
@@ -0,0 +1,12 @@
+cat escape_sequance.py
+#example for \ escape charater
+print ("hello \"abc\" world")
+print ('hello \'abc\' world')
+
+# example for new line with  \n
+print ("hello \"abc\" world \n"'hello \'abc\'world')
+
+#example for tabe for \t
+
+print("hello \"Welcome\" Henkel\n"'\t''hello \'welcome\' Henkel\n')
+root@server:~/Python_Scripts#
