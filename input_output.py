bb989c397f5a6984a5ba2d129204bd24cf76842e
Gauav Rege <gauravarege@gmail.com>
2020-10-01 22:55:58 +0530

    Add files via upload

---
+++ input_output.py
@@ -0,0 +1,9 @@
+'''
+if you want to result in INT(intger) so we
+need to put  int before enter your value.
+it's work only on Python3.
+'''
+a=int(input("enter your Value"))
+b=int(input("enter your value"))
+c=a+b
+print(c)
